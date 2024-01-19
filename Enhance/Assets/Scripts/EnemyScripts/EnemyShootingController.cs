64277c6d0a803a91cc68790fc125abb372cb3f0b
EgorF <egor1268@gmail.com>
2024-01-19 16:01:46 +0500

    refactored to use base Shooter class for all shooters

--- Enhance/Assets/Scripts/EnemyScripts/EnemyShootingController.cs
+++ Enhance/Assets/Scripts/EnemyScripts/EnemyShootingController.cs
@@ -2,28 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyShootingController : MonoBehaviour
+public class EnemyShootingController : Shooter
 {
-    [SerializeField] private GameObject _bullet;
-    [SerializeField] private Transform _bulletInitialPosition;
-    [SerializeField] private float shootCooldown = 2f;
-
-    private float timer = 0f;
-
-    void Update()
-    {
-        timer += Time.deltaTime;
-
-        // shoot periodically
-        if (timer > shootCooldown)
-        {
-            timer = 0;
-            Shoot(_bullet, _bulletInitialPosition);
-        }
-    }
-
-    private void Shoot(GameObject bulletPrefab, Transform bulletInitialPos)
-    {
-        Instantiate(bulletPrefab, bulletInitialPos.position, Quaternion.identity);
-    }
 }
--- Enhance/Assets/Scripts/Shooter.cs
+++ Enhance/Assets/Scripts/Shooter.cs
@@ -4,6 +4,26 @@ using UnityEngine;
 
 public abstract class Shooter : MonoBehaviour 
 {
-    public abstract Transform GetTarget();
-    protected abstract void Shoot(GameObject bulletPrefab, Transform bulletInitialPos);
+    [SerializeField] private GameObject _bullet;
+    [SerializeField] private Transform _bulletInitialPosition;
+    [SerializeField] private float shootCooldown = 2f;
+
+    private float timer = 0f;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        // shoot periodically
+        if (timer > shootCooldown)
+        {
+            timer = 0;
+            Shoot(_bullet, _bulletInitialPosition);
+        }
+    }
+
+    private void Shoot(GameObject bulletPrefab, Transform bulletInitialPos)
+    {
+        Instantiate(bulletPrefab, bulletInitialPos.position, Quaternion.identity);
+    }
 }
