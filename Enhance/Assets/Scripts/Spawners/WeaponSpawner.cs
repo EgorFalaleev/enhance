3e19e147b6ff158542ffbbfe5fb084486d51563e
EgorF <egor1268@gmail.com>
2024-01-21 15:28:31 +0500

    implemented weapons spawner

--- Enhance/Assets/Scripts/Spawners/WeaponSpawner.cs
+++ Enhance/Assets/Scripts/Spawners/WeaponSpawner.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WeaponSpawner : MonoBehaviour
+public class WeaponSpawner : Spawner
 {
-    [SerializeField] private List<GameObject> _weaponPrefabs;
+    [SerializeField] private float _timeTillNextWeaponSpawn = 5f;
+
+    private float _timer = 0f;
 
     void Start()
     {
-        
+        SpawnRandomObject();
     }
 
-    void Update()
+    protected override void Update()
     {
-        
+        base.Update();
+
+        _timer += Time.deltaTime;
+
+        if (_timer > _timeTillNextWeaponSpawn)
+        {
+            _timer = 0f;
+            SpawnRandomObject();
+        }
     }
 }
