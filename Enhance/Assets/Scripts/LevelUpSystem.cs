cf1ddc22c4e69114ac61138f426122c97ffbb656
EgorF <egor1268@gmail.com>
2024-01-22 12:21:56 +0500

    added methods for changing exp and lvl

--- Enhance/Assets/Scripts/LevelUpSystem.cs
+++ Enhance/Assets/Scripts/LevelUpSystem.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 
 public class LevelUpSystem 
 {
+    public event EventHandler OnExperienceChanged;
+    public event EventHandler OnLevelChanged;
+
     private int _level;
     private int _experience;
     private int _requiredExpToNextLevel;
@@ -17,6 +20,36 @@ public class LevelUpSystem
         _level = 1;
         _experience = 0;
         _requiredExpToNextLevel = CalculateRequiredExp(_level);
+    }
+
+    public int GetLevel()
+    {
+        return _level;
+    }
+
+    public void AddExperience(int amount)
+    {
+        _experience += amount;
+
+        if (_experience >= _requiredExpToNextLevel) 
+        {
+            LevelUp();
+        }
+
+        // notify subscribers about exp change
+        if (OnExperienceChanged != null)
+            OnExperienceChanged(this, EventArgs.Empty);
+    }
+
+    private void LevelUp()
+    {
+        _level++;
+        _experience -= _requiredExpToNextLevel;
+        _requiredExpToNextLevel = CalculateRequiredExp(_level);
+
+        // notify subscribers about lvl change
+        if (OnLevelChanged != null) 
+            OnLevelChanged(this, EventArgs.Empty);
     }
 
     // equation for required exp calculation from https://oldschool.runescape.wiki/w/Experience
