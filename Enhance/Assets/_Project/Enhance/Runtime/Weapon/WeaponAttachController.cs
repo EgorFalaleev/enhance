38454228b0d918c1eb65d18623fc606366faef7e
EgorF <egor1268@gmail.com>
2024-02-19 20:47:33 +0500

    refactored weapon classes to use configs

--- Enhance/Assets/_Project/Enhance/Runtime/Weapon/WeaponAttachController.cs
+++ Enhance/Assets/_Project/Enhance/Runtime/Weapon/WeaponAttachController.cs
@@ -1,28 +1,45 @@
 using System;
+using Enhance.Data;
 using UnityEngine;
 
 namespace Enhance.Runtime.Weapon
 {
-    public class WeaponAttachController : MonoBehaviour
+    public class WeaponAttachController : MonoBehaviour, IDamageable
     {
+        [SerializeField] private WeaponConfigSO _weaponConfig;
+
         public event EventHandler OnDie;
-        public bool IsAttached { get { return _isAttached;} }
+        
+        public int CurrentHealth { get; private set; }
 
         private const float WEAPON_ATTACH_MODIFIER = 1.25f;
         private const float PLAYER_ATTACH_MODIFIER = 0.9f;
 
-        private bool _isAttached = false;
+        private bool _isAttached;
+        private float _timer = 0f;
+
+        private void Start()
+        {
+            CurrentHealth = _weaponConfig.MaxHealth;
+        }
+
+        private void Update()
+        {
+            if (_isAttached)
+                return;
+
+            _timer += Time.deltaTime;
+            
+            if (_timer >= _weaponConfig.LifeTime)
+                Die();
+        }
 
         private void OnTriggerEnter2D(Collider2D collision)
         {
             // enemy contact with weapon destroys it
             if (collision.CompareTag(Tags.ENEMY) || collision.CompareTag(Tags.ENEMY_PROJECTILE))
             {
-                if (OnDie != null)
-                    OnDie(this, EventArgs.Empty);
-
-                Destroy(gameObject);
-                return;
+                TakeDamage(CurrentHealth);
             }
 
             // can attach only once 
@@ -33,18 +50,35 @@ namespace Enhance.Runtime.Weapon
 
                 // attach to a collision GO
                 transform.SetParent(collision.transform, false);
-                transform.localPosition = collision.tag == Tags.WEAPON ? direction.normalized * WEAPON_ATTACH_MODIFIER: direction.normalized * PLAYER_ATTACH_MODIFIER;
+                transform.localPosition = collision.CompareTag(Tags.WEAPON) ? direction.normalized * WEAPON_ATTACH_MODIFIER: direction.normalized * PLAYER_ATTACH_MODIFIER;
 
                 // change scale to 1 if attached to another weapon to prevent getting smaller
-                if (collision.tag == Tags.WEAPON)
+                if (collision.CompareTag(Tags.WEAPON))
                     transform.localScale = Vector3.one;
 
                 _isAttached = true;
 
                 // weapon can shoot now
-                GetComponent<WeaponShooter>().IsWeaponAttached = true;
+                GetComponentInChildren<WeaponShooter>().IsWeaponAttached = true;
             }
         }
 
+        public void TakeDamage(int amount)
+        {
+            CurrentHealth -= amount;
+
+            if (CurrentHealth <= 0)
+            {
+                Die();
+            }
+        }
+
+        public void Die()
+        {
+            if (OnDie != null)
+                OnDie(this, EventArgs.Empty);
+            
+            Destroy(gameObject);
+        }
     }
 }
--- Enhance/Assets/_Project/Enhance/Runtime/Weapon/WeaponShooter.cs
+++ Enhance/Assets/_Project/Enhance/Runtime/Weapon/WeaponShooter.cs
@@ -1,14 +1,68 @@
+using Enhance.Data;
+using UnityEngine;
+
 namespace Enhance.Runtime.Weapon
 {
-    public class WeaponShooter : Shooter
+    public class WeaponShooter : MonoBehaviour, IShooter
     {
+        [SerializeField] private WeaponConfigSO _weaponConfig;
+        [SerializeField] private BulletConfigSO _bulletConfig;
+        
         public bool IsWeaponAttached { get; set; }
+
+        private float _timer = 0f;
+        private Transform _target;
 
         private void Update()
         {
-            // weapon can shoot only if attached to player
-            if (IsWeaponAttached)
-                ShootWithCooldown(_bullet, _bulletInitialPosition, _shootCooldown);
+            // dont't shoot if weapon is not attached
+            if (!IsWeaponAttached)
+                return;
+            
+            _timer += Time.deltaTime;
+
+            if (_timer > _weaponConfig.ShootingCooldown)
+            {
+                _timer = 0f;
+                Shoot(_bulletConfig, _target, transform);
+            }
+        }
+
+        public void Shoot(BulletConfigSO bulletConfig, Transform target, Transform shootPosition)
+        {
+            if (!FindAndSetClosestEnemy())
+                return;
+            
+            ObjectPoolingManager.SpawnObject(bulletConfig.BulletPrefab, shootPosition.position, Quaternion.identity);
+        }
+        
+        private bool FindAndSetClosestEnemy()
+        {
+            // find all enemies within radius
+            // TODO: replace with OverlapCircle()
+            var enemyInRangeColliders = Physics2D.OverlapCircleAll(transform.position, _weaponConfig.AttackRange, LayerMask.GetMask("Enemy"));
+        
+            float shortestDistance = Mathf.Infinity;
+
+            if (enemyInRangeColliders.Length > 0)
+            {
+                foreach (var enemy in enemyInRangeColliders)
+                {
+                    var distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+
+                    // update closest enemy
+                    if (distanceToEnemy < shortestDistance)
+                    {
+                        shortestDistance = distanceToEnemy;
+                        _target = enemy.gameObject.transform;
+                    }
+                }
+            }
+            // no enemies in visible range
+            else
+                return false;
+
+            return true;
         }
     }
 }
